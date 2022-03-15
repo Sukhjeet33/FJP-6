8d57c423f7d54f2c5c1a0440430c8f13f88311c9
Sukhjeet33 <sukhjeet33@gmail.com>
2022-03-15 18:17:48 +0530

    read,update,delete

--- Module-2/IntroToNode/fs.js
+++ Module-2/IntroToNode/fs.js
@@ -6,6 +6,20 @@ let path = require("path");
 let filePath = path.join(__dirname, "file.txt");
 // console.log(filePath);
 
+// CRUD
+// create
 fs.writeFileSync(filePath, "Writing on existing file!!!!!!");
 
-// fs.readFileSync(filePath);
+// read
+let content = fs.readFileSync(filePath, "utf-8");
+console.log(content);
+
+// update
+
+fs.appendFileSync(filePath, "\nAdding more content??!!!");
+
+console.log(fs.readFileSync(filePath, "utf-8"));
+
+// delete
+
+fs.unlinkSync(filePath);
