49091895a4641535b0f94cdaf38f204e68e2386c
Sukhjeet33 <sukhjeet33@gmail.com>
2022-03-15 16:59:05 +0530

    Path

--- Module-2/IntroToNode/path.js
+++ Module-2/IntroToNode/path.js
@@ -1,5 +1,6 @@
 // "/Users/Sukhi/pepCoding/DevFolder/FJP-6/Module-2/introToNode/path.js"
 
+const { dirname } = require("path");
 let path = require("path");
 console.log(path);
 
@@ -12,3 +13,10 @@ let baseName = path.basename(
   "/Users/Sukhi/pepCoding/DevFolder/FJP-6/Module-1/css_selector_questions/q1.html"
 );
 console.log(baseName);
+
+console.log(__dirname);
+
+console.log(__filename);
+
+let newFilePath = path.join(__dirname, "testt.js");
+console.log(newFilePath);
