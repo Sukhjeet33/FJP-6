b893170a106325914b57a621e3b4b969f36b0ac4
Sukhjeet33 <sukhjeet33@gmail.com>
2022-03-15 16:41:47 +0530

    JavaScript and Node Intro

---
+++ Module-2/IntroToNode/path.js
@@ -0,0 +1,14 @@
+// "/Users/Sukhi/pepCoding/DevFolder/FJP-6/Module-2/introToNode/path.js"
+
+let path = require("path");
+console.log(path);
+
+let extentionName = path.extname(
+  "/Users/Sukhi/pepCoding/DevFolder/FJP-6/Module-2/introToNode/path.js"
+);
+console.log(extentionName);
+
+let baseName = path.basename(
+  "/Users/Sukhi/pepCoding/DevFolder/FJP-6/Module-1/css_selector_questions/q1.html"
+);
+console.log(baseName);
