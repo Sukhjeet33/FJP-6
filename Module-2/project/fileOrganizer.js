514c4e33f9510de07e787c3b1f598d7babed0fbe
Sukhjeet33 <sukhjeet33@gmail.com>
2022-03-17 21:58:51 +0530

    download folder created

--- Module-2/project/fileOrganizer.js
+++ Module-2/project/fileOrganizer.js
@@ -1,5 +1,18 @@
-let inputArr = process.argv;
-console.log(inputArr);
+// let inputArr = process.argv;
+// console.log(inputArr);
 
-let input = process.argv[2];
-console.log(input);
+// let input = process.argv[2];
+// console.log(input);
+
+let fs = require("fs");
+
+let folderPath = process.argv[2];
+
+let folderExists = fs.existsSync(folderPath);
+
+if (folderExists) {
+  let files = fs.readdirSync(folderPath);
+  console.log(files);
+} else {
+  console.log("Enter a valid Path !!!");
+}
